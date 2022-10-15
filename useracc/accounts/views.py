87f6d7758993bc7d10c6807ab5d5c42b3c7dae8f
hites53 <hitesh53gharat@gmail.com>
2022-10-16 02:09:48 +0530

    Now we have working piece of authentication

--- useracc/accounts/views.py
+++ useracc/accounts/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
+
+def display(request):
+    return render(request,'accounts/display.html')
+
 def signup(request):
     if request.method == "POST":
         if request.POST['password1'] == request.POST['password2']:
@@ -12,7 +16,7 @@ def signup(request):
             except User.DoesNotExist:
                 user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
                 auth.login(request,user)
-                return redirect('home')
+                return redirect('display')
         else:
             return render (request,'accounts/signup.html', {'error':'Password does not match!'})
     else:
@@ -23,14 +27,14 @@ def login(request):
         user = auth.authenticate(username=request.POST['username'],password = request.POST['password'])
         if user is not None:
             auth.login(request,user)
-            return redirect('home')
+            return redirect('display')
         else:
             return render (request,'accounts/login.html', {'error':'Username or password is incorrect!'})
     else:
         return render(request,'accounts/login.html')
 
 def logout(request):
-    if request.method == 'POST':
-        auth.logout(request)
-    return redirect('home')
+    # if request.method == 'POST':
+    auth.logout(request)
+    return redirect('display')
 
